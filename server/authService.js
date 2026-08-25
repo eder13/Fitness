521d0dcea6b1c2eaf38006a77eb517427ed2f785
eder13 <simon.ranftl82@gmail.com>
2026-08-25 15:14:13 +0200

    fix: nonce error authentication on re-login

--- server/authService.js
+++ server/authService.js
@@ -42,9 +42,6 @@ class AuthService {
             throw new Error('ID token nonce validation failed');
         }
 
-        // Nonce is only used for login callback and then discarded
-        delete request.session.nonce;
-
         await this._createJWETokenCookie(authenticationResult, response);
     }
 
